dbbbf45a7c0f979a8d6c7ed9fea9adc29b3222e1
nitslaszlo <nitslaszlo@gmail.com>
2022-02-26 21:50:13 +0100

    up

--- src/VueMastery_Socks/types/IVariant.ts
+++
@@ -1,6 +0,0 @@
-export default interface IVariant {
-  id: number;
-  color: string;
-  image: string;
-  quantity: number;
-}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
-import "mdb-vue-ui-kit/css/mdb.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 import App from "./App.vue";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import i18n from "./plugins/i18n";
 import routes from "./routes";
-import vuetify from "./plugins/vuetify";
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(routes);
-app.use(i18n);
-app.use(vuetify);
 
 app.mount("#app");
